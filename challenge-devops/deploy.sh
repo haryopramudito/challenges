fa9700e7cd14fc9f96f80cdff4334f8a7bca3fd3
Haryo Pramudito <haryo@localhost.localdomain>
2022-05-16 20:03:16 +0700

    completed challenge

---
+++ challenge-devops/deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+export BASE_PATH=`pwd`
+export HELM_PATH=$BASE_PATH/helm
+if [ -z "$1" ]
+        then
+        echo "#################################################"        
+        echo "# please add environment argument !!            #"
+        echo "# =================================             #"
+        echo "# sh scriptname.sh dev or sh scriptname.sh prod #"
+        echo "#################################################"
+else
+        export ENV=$1
+        define_env()
+        {
+                export NAMESPACE=opn-$ENV
+                #please specify your kubeconfig file in my case i use as below file
+                export KUBECONFIG=~/.kube/config_$ENV
+        }
+
+        deploy_db()
+        {
+                cd $HELM_PATH/patroni
+                if [ "$ENV" = "prod" ]; then
+                       helm upgrade --install --atomic --timeout 30s --values values-$ENV.yaml --namespace  $NAMESPACE patroni . 
+
+                else
+                        helm upgrade --install --atomic --timeout 30s --values values-$ENV.yaml --namespace  $NAMESPACE patroni .
+                fi
+        }
+
+                deploy_apps()
+        {
+                cd $HELM_PATH/opn-devops
+                if [ "$ENV" = "prod" ]; then
+                         helm upgrade --install --atomic --timeout 30s --values values-$ENV.yaml $NAMESPACE opn-devops . 
+
+                else
+                        helm upgrade --install --atomic --timeout 30s --values values-$ENV.yaml --namespace  $NAMESPACE opn-devops . 
+                fi
+        }
+
+        echo "define Environment Variables:"
+        echo =================================
+        define_env
+        echo ""
+        echo "Deploy DB Using Helm Chart"
+        echo =================================
+        #deploy_db
+        echo ""
+        echo "Deploy Apps Using Helm Chart"
+        echo =================================
+        deploy_apps
+fi
+
